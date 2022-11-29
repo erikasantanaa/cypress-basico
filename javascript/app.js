b9555685495b5b6ef6e8bd12b0e7169f36f6a06e
erikasantanaa <erikamariasantanaa@gmail.com>
2022-11-29 00:05:06 -0300

    Curso Cypress

--- javascript/app.js
+++ javascript/app.js
@@ -1,3 +1,21 @@
+
+
+let avengers = ['Tony Stark', 'Clint Barton', 'Natasha Romanoff', 'Steve Rogers']
+
+function listaVingadores() {
+
+    let ul = document.getElementById('avengers')
+    ul.innerHTML = ''
+
+    avengers.forEach(function(a){
+        let li = document.createElement('li')
+        let text = document.createTextNode(a)
+        li.appendChild(text)
+        ul.appendChild(li)
+    })
+
+}
+
 function verificarIdade() {
 
     let nome = document.querySelector('input[name=nome]')
