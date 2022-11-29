af622840e57d7517b11bf4e0fa159733d365b677
erikasantanaa <erikamariasantanaa@gmail.com>
2022-11-28 22:42:02 -0300

    Se inicia o curso de Cypress básico com um teste básico e estudos em Javascript

---
+++ javascript/app.js
@@ -0,0 +1,20 @@
+function verificarIdade() {
+
+    let nome = document.querySelector('input[name=nome]')
+    let idade = document.querySelector('input[name=idade')
+
+    if (idade.value.length == 0 ) {
+        alert( 'Idade é um campo obrigatório!')
+        return
+    }
+
+    let idadeNum = parseInt(idade.value)
+
+    if (idadeNum >= 18) {
+        alert('Ok, você pode tirar sua CNH')
+    } else if (idadeNum > 4) {
+        alert('Você é menor de idade, por enquanto sugiro andar de bike')
+    } else {
+        alert('Melhor você ir andar de andajar')
+    }
+}
