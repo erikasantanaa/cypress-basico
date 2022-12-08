b8a9bcc374b5ad7b3495f56f67fde98b278f2812
erikasantanaa <erikamariasantanaa@gmail.com>
2022-12-08 16:26:30 -0300

    testes Cadastro de entregador

---
+++ buger-eats/cypress/e2e/cadastro.cy.js
@@ -0,0 +1,16 @@
+describe('Cadastro', ()=> {
+  it('usuário deve se tornar um entregador', ()=> {
+    cy.viewport(1440, 900)
+    cy.visit('https://buger-eats.vercel.app')
+
+    cy.get('a[href="/deliver"]').click()
+    cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas')
+  })
+})
+
+
+describe('Cadastro', () => {
+  it('Usuário deve se tornar um entregador', () => {
+    
+  })
+})
